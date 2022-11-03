2313fdfb8acac34dddcceb43043b55da368533c2
Admin <Admin@DESKTOP-SPENILP>
2022-11-03 20:10:55 +0530

    First Commit

---
+++ JavaProject/src/pack1/check.java
@@ -0,0 +1,23 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class check {
+	
+	public static void main(String[]args)
+	{
+	int n;
+	System.out.println("Enter Number");
+	Scanner r = new Scanner(System.in);
+	n=r.nextInt();
+	if(n>0)
+	{
+	System.out.println("positive number");
+	}
+	else if (n<0)
+	{
+	System.out.println("negative number");
+	}
+	}}
+
+
